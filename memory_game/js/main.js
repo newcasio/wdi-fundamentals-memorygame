0c9ea316935929d60f06bd8bbbf20b362731f3e1
Danith Thai <braddong@hotmail.com>
2018-09-09 16:32:28 +1000

    not working yet

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -19,6 +19,8 @@ var cards =[{
 var cardsInPlay = [];
 
 var checkForMatch = function(){
+	this.setAttribute('src', cards[cardId].cardImage);
+
 	if (cardsInPlay.length===2){
 		if(cardsInPlay[0]===cardsInPlay[1]){
 			alert ('You found a match');
@@ -29,8 +31,8 @@ var checkForMatch = function(){
 
 };
 
-
-var flipCard = function(cardId){
+var flipCard = function(){
+	var cardId = this.getAttribute('data-id');
 
 	console.log(`User flipped ${cards[cardId].rank}`);
 	console.log(cards[cardId].cardImage);
@@ -42,6 +44,14 @@ var flipCard = function(cardId){
 
 };
 
-flipCard(0);
-flipCard(1);
+var createBoard = function(){
+	for (var i =0; i<cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement); 
+	}
+};
 
+createBoard();
